c58aef5d08c5247a76d980e7d500d30595810740
Kaushal Kishor <kaushal1214@gmail.com>
2023-07-03 01:47:10 +0530

    user2 fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-console.log("Issue fiex")
+console.log("User2 fixed it differently");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
